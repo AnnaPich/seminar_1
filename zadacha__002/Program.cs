7af264c521a29737584ee5e289e611dc372fe2ec
Anna <anek_ogonek@mail.ru>
2022-11-27 16:22:56 +0300

    added file zadacha_002

---
+++ zadacha__002/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.Write("Введите целое число 1: ");
+int number_1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите целое число 2: ");
+int number_2 = Convert.ToInt32(Console.ReadLine());
+
+int square = number_2 * number_2;
+if(number_1 == square)// проверка условия равенства 
+{
+    Console.Write($"Число {number_1} равно квадрату числа {number_2}");
+}
+else
+{
+    Console.Write($"Число {number_1} не равно квадрату числа {number_2}");
+}
+
