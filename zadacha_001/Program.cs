d0636f6bef2c1cef5db99a2415ade45940ac3925
Anna <anek_ogonek@mail.ru>
2022-11-27 15:10:39 +0300

    added file zadacha_001

---
+++ zadacha_001/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Clear();
+Console.Write("Введите целое число: ");
+int number = int.Parse(Console.ReadLine());
+int square = number * number;
+Console.Write ("Квадрат числа " + number + " равен: ");
+Console.WriteLine(square);
