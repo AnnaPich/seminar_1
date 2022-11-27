b98f10bb71f733357cd74ce14569015a90b2ebc5
Anna <anek_ogonek@mail.ru>
2022-11-27 16:40:30 +0300

    added new file zadacha_003

---
+++ zadacha_003/Program.cs
@@ -0,0 +1,26 @@
+﻿//Задача 2:
+//Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+// 3-> среда
+// 5-> пятница
+
+Console.Clear();
+Console.Write("Введите целое число от 1 до 7: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number == 3) 
+{
+    Console.Write("Сегодня среда");
+}
+else
+{
+    if(number == 5)
+    {
+        Console.Write("Сегодня пятница");   
+    }
+     else
+    {
+        Console.Write("Введите другое число");
+    }   
+}
+
+
