b4fa5b6d1d1b0ba55ab53c57949a5c5dcc4e1cc1
Anna <anek_ogonek@mail.ru>
2022-11-27 15:55:20 +0300

    added new comment for zadacha_001

--- zadacha_001/Program.cs
+++ zadacha_001/Program.cs
@@ -2,4 +2,4 @@
 Console.Write("Введите целое число: ");
 int number = int.Parse(Console.ReadLine());
 int square = number * number;
-Console.Write ($"Квадрат числа {number} равен: {square}");
+Console.Write ($"Квадрат числа {number} равен: {square}");//другая форма записи
