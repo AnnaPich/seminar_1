5cba1ccf5f4c096edfdcaaf9d264a88566e06fc7
Anna <anek_ogonek@mail.ru>
2022-12-03 14:18:59 +0300

    added zadacha_008 seminar2

---
+++ zadacha_008/Program.cs
@@ -0,0 +1,17 @@
+﻿//Семинар 2 Задача 3(14)
+// Напишите программу, которая принимает на вход число
+//и проверяет, кратно ли оно одновременно 7 и 23.
+
+Console.Clear();
+
+Console.Write("Введите целое число:");
+int number = int.Parse(Console.ReadLine());
+
+if(number % 7 == 0 && number % 23 == 0)// && - знак "И" между условиями
+{
+    Console.WriteLine($"{number} -> да");
+}
+else
+{
+    Console.WriteLine($"{number} -> нет");
+}
